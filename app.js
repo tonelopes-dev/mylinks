0b8fd5676c4c48c964c019e8fb2c5ee97f4a1606
Tone Lopes Monte <119428578+ToneLopes@users.noreply.github.com>
2023-03-07 15:36:37 -0300

    Fazendo o primeiro commit da arvore de links

---
+++ app.js
@@ -0,0 +1,46 @@
+
+
+
+document.querySelector("#switch").onclick = function toggleMode() {
+  const html = document.documentElement;
+
+  html.classList.toggle("light");
+
+  /* if(html.classList.contains("light")){
+        html.classList.remove('light')
+    } else {
+        html.classList.add('light')
+    }  esse código foi substituido por Toggle*/
+
+  // pegar a imagem do perfil
+  const img = document.querySelector("#profile img");
+
+  //se tiver não light mode, adicionar a imagem light
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/img/avatar-photo/avatar-light.jpg");
+    img.setAttribute(
+      "alt",
+      "Foto de perfil de Tone usando óculos de grau em um dia de sol."
+    );
+
+    localStorage.setItem('light', 1)
+  } else {
+    img.setAttribute("src", "./assets/img/avatar-photo/avatar-dark.jpg");
+    img.setAttribute(
+      "alt",
+      "Foto de perfil de Tone usando toca e moletam na Time Squere em Nova Iork."
+    );
+    localStorage.setItem('light', 2)
+    
+  }
+  
+};
+
+function loadTheme(){
+  let lightMode = localStorage.getItem('light', 1)
+  const html = document.documentElement;
+
+  if (lightMode.contains("light", 2)){
+    html.classList.toggle('light')
+  } 
+}
---
+++ displayOn.js
@@ -0,0 +1,7 @@
+setTimeout(displayOn, 1500)
+
+function displayOn () {
+    const container = document.querySelector('#container')
+    container.classList.add('displayOn')
+    }
+    
